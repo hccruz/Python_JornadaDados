65d18638fb4ed25bd227549f55cad972c2d34099
Heraldo Cruz <hccruz@gmail.com>
2025-09-12 23:26:04 -0300

    Update of the program kpi.py

--- Aula01/kpi.py
+++ Aula01/kpi.py
@@ -2,6 +2,9 @@
 
 # Calcular o salário total de um usuário com base no salário e bônus fornecidos
 
+# Constante do bônus fixo
+BONUS_FIXO = 1000
+
 # Entre com o nome do usuário.
 nome = input('Digite o nome do usuário: ')
 
@@ -12,7 +15,7 @@ salario = float(input('Digite o salário do usuário: '))
 bonus = float(input('Digite o bônus do usuário: '))
 
 # Calcule o salário total.
-salario_total = 1000 + salario * bonus
+salario_total = BONUS_FIXO + salario * bonus
 
 # Exiba o resultado.
 print(f'Olá {nome}, o seu bônus foi de R${salario_total:.2f}')
