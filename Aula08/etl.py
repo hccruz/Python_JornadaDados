d534710879b1fd38ae99a4f4a9258c97e22bd76e
Heraldo Candido da Cruz <hccruz@gmail.com>
2026-05-16 14:10:20 -0300

    Salvando o projeto

--- Aula08/etl.py
+++ Aula08/etl.py
@@ -1,13 +1,39 @@
-# import pandas as pd
+import pandas as pd
 import os
 import glob
 
-# Uma função de extract que lê e consolida os jsons
 
-pasta = '/media/heraldo/Arquivos/Documentos/JornadaDados/Python_JornadaDados/Aula08/data'
-arquivos_json = glob.glob(os.path.json(pasta, '*.json'))
-print(arquivos_json)
+# Função de extract que lê e consolida os jsons
+def extrair_dados_consolidar(pasta: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
 
-# Uma função que transforma
 
-# Uma função que da load em csv ou parquet
+# Função que transforma
+def calcular_kpi_total_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df['Total'] = df['Quantidade'] * df['Venda']
+    return df
+    
+
+# Função que da load em csv ou parquet
+def carregar_dados(df: pd.DataFrame, format_exit: list):
+    '''
+    Parâmetro que vai ser ou "csv" ou "parquet" ou "os dois"
+    '''
+    
+    for formato in format_exit:
+        if formato == 'csv':
+            df.to_csv('dados.csv', index=False)
+        if formato == 'parquet':
+            df.to_parquet('dados.parquet', index=False)
+
+
+# Função para o pipeline da etl
+def pipeline_calcular_kpi_vendas_consolidado(pasta: str, formato_saida: list):
+    data_frame = extrair_dados_consolidar(pasta)
+    data_frame_calculado = calcular_kpi_total_vendas(data_frame)
+    carregar_dados(data_frame_calculado, formato_saida)
+
+    
