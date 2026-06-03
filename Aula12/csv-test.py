66dae78a4b78f28412137737611b370ac147a332
Heraldo Candido da Cruz <hccruz@gmail.com>
2026-06-02 22:25:05 -0300

    Atualizando a pasta

--- Aula12/csv-test.py
+++ Aula12/csv-test.py
@@ -5,4 +5,14 @@ df = pd.read_csv('./exemplo.csv')
 
 df_filtrado = df[df['estado'] == 'SP']
 
+df_filtrado = df[df['preço'] == '10,50']
+
+print(df_filtrado)
+
+df2 = pd.read_csv('./exemplo2.csv')
+
+df_filtrado2 = df2[df2['estado'] == 'DF']
+
+df_filtrado2 = df2[df2['preço'] == '10,50']
+
 print(df_filtrado)
